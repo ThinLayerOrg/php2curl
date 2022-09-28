02efec29bc8b4f7306858b623a7e33ace139c426
Ilia Yatsenko <ilia.yatsenko@gmail.com>
2022-09-28 15:06:48 +0300

    adds an ability to override some values

--- src/Php2Curl.php
+++ src/Php2Curl.php
@@ -4,15 +4,15 @@ namespace ThinLayer\Php2Curl;
 
 class Php2Curl
 {
-    public static function build(): string
+    public static function build(?string $host = null, ?int $port = null, ?string $scheme = null, ?string $method = null): string
     {
         return (new CurlBuilder())
             ->headers(getallheaders() ?? [])
             ->input(file_get_contents('php://input'))
-            ->method($_SERVER['REQUEST_METHOD'])
-            ->scheme($_SERVER['REQUEST_SCHEME'])
-            ->host($_SERVER['SERVER_NAME'])
-            ->port($_SERVER['SERVER_PORT'])
+            ->method($method ?? $_SERVER['REQUEST_METHOD'])
+            ->scheme($scheme ?? $_SERVER['REQUEST_SCHEME'])
+            ->host($host ?? $_SERVER['SERVER_NAME'])
+            ->port($port ?? $_SERVER['SERVER_PORT'])
             ->uri($_SERVER['REQUEST_URI'])
             ->post($_POST)
             ->build();
