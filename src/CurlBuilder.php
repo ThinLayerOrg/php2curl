b904ac13374b39c4d102f2b4666b76eef1ff4061
Ilia Yatsenko <ilia.yatsenko@gmail.com>
2022-09-28 18:04:48 +0300

    simplifies logic

--- src/CurlBuilder.php
+++ src/CurlBuilder.php
@@ -14,15 +14,26 @@ class CurlBuilder
     private const CONTENT_TYPE_FORM_URL_ENCODED = 'application/x-www-form-urlencoded';
     private const CONTENT_TYPE_RAW = 'raw';
 
-    private string $method = 'GET';
-    private string $scheme = 'http';
-    private string $host = 'localhost';
-    private int $port = self::HTTP_PORT;
-    private string $uri = '/';
-    private array $post = [];
-    private array $headers = [];
-    private array $preparedHeaders = [];
-    private ?string $input = null;
+    private string $method;
+    private string $scheme;
+    private string $host;
+    private int $port;
+    private string $uri;
+    private array $post;
+    private array $headers;
+    private ?string $input;
+
+    public function __construct()
+    {
+        $this->headers = getallheaders();
+        $this->input = file_get_contents('php://input');
+        $this->method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+        $this->scheme = $_SERVER['REQUEST_SCHEME'] ?? 'http';
+        $this->host = $_SERVER['SERVER_NAME'] ?? 'localhost';
+        $this->port = $_SERVER['SERVER_PORT'] ?? 80;
+        $this->uri = $_SERVER['REQUEST_URI'] ?? '/';
+        $this->post = $_POST;
+    }
 
     public function method(string $method): self
     {
@@ -96,9 +107,9 @@ class CurlBuilder
         return $headers;
     }
 
-    private function guessContentType(): string
+    private function guessContentType(array $headers): string
     {
-        $contentType = $this->preparedHeaders[self::HEADER_CONTENT_TYPE] ?? '';
+        $contentType = $headers[self::HEADER_CONTENT_TYPE] ?? '';
         if (stripos($contentType, self::CONTENT_TYPE_FORM_DATA) !== false) {
             return self::CONTENT_TYPE_FORM_DATA;
         }
@@ -128,7 +139,7 @@ class CurlBuilder
                 return '';
         };
 
-        $contentType = $this->guessContentType();
+        $contentType = $this->guessContentType($this->headers);
         switch ($contentType) {
             case self::CONTENT_TYPE_FORM_DATA:
                 return $this->buildFormDataBody();
